e4129856b9441ba90f1d03bf7c67d97562a653c9
MahaAswin <mahaaswinsb@gmail.com>
2026-07-26 11:03:22 +0530

    Initial Spring Boot backend setup

---
+++ backend/src/main/java/com/gosafe/backend/GosafeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gosafe.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosafeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GosafeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gosafe/backend/GosafeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosafe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosafeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
